16afca57b541b794c8712b0f5c611fad68f65e9e
48089748z <48089748z@iespoblenou.org>
2016-03-09 16:55:11 +0100

    Commit

--- src/UF3PSP/ExamenUF3PSP/Exercici1/ServerController.java
+++ src/UF3PSP/ExamenUF3PSP/Exercici1/ServerController.java
@@ -1,8 +1,9 @@
-package UF3PSP.ServidorWEB;
+package UF3PSP.ExamenUF3PSP.Exercici1;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+
 public class ServerController
 {
     public static void main(String[] args)
@@ -10,12 +11,12 @@ public class ServerController
         try
         {
             ServerSocket serverSocket = new ServerSocket();
-            InetSocketAddress address = new InetSocketAddress("0.0.0.0", 9090);
+            InetSocketAddress address = new InetSocketAddress("0.0.0.0", 9090); //LE DECIMOS QUE SERA ACCESIBLE EN EL PUERTO 9090
             serverSocket.bind(address);
-            Socket socket = serverSocket.accept();
+            Socket socket = serverSocket.accept(); //BIND I ACCEPT
             ServerThread thread = new ServerThread(socket);
-            thread.start();
+            thread.start(); //INICIAMOS EL THREAD
 
-        } catch (IOException e) {}
+        } catch (IOException e) {e.printStackTrace();}
     }
 }
---
+++ src/UF3PSP/ExamenUF3PSP/Test.java
@@ -0,0 +1,7 @@
+package UF3PSP.ExamenUF3PSP;
+
+/**
+ * Created by 48089748z on 09/03/16.
+ */
+public class Test {
+}
