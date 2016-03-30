12b4ec9f151edc640c9dbca40ea3d419b94fd686
48089748z <48089748z@iespoblenou.org>
2016-03-30 16:29:20 +0200

    Commit

--- src/UF4AAD/Controller.java
+++ src/UF4AAD/Controller.java
@@ -6,44 +6,40 @@ import javax.xml.xquery.XQException;
  */
 public class Controller
 {
-    private static final DAO DAO = new DAO();
+    private static final ExistsDAO existsDAO = new ExistsDAO();
 
-    //AMB AQUEST DAO NOMES CAL CAMBIAR LES PREFERENCIES PER QUE FUNCIONI AMB QUALSEVOL ALTRE USUARI I COLECCIÓ
-    private static String adminUsername = "admin";
-    private static String adminPassword = "dionis";
-    private static String filePath = "/home/48089748z/Escriptori/IdeaProjects/DadesIServeis/src/UF3AAD/ExamenUF3/mondial.xml";
-    private static String mainCollection ="/db";
-    private static String yourCollection = "uriDAO";
-    private static String IP = "172.31.101.225";
+    //AMB AQUEST ExistsDAO NOMES CAL CAMBIAR LES PREFERENCIES PER QUE FUNCIONI AMB QUALSEVOL ALTRE USUARI I COLECCIÓ
+    private static final String IP = "172.31.101.225";
     private static final String PORT = "8080";
     private static final String URI = "xmldb:exist://"+IP+":"+ PORT +"/exist/xmlrpc";
-    private static final String driver = "org.exist.xmldb.DatabaseImpl";
 
-    //EL CODI DEL MAIN NO CAL TOCARLO I EL DEL DAO TAMPOC
+    private static String adminUsername = "admin";
+    private static String adminPassword = "dionis";
+
+    private static String mainCollection ="/db";
+    private static String yourCollection = "uriDAO";
+
+    private static String filePath = "/home/48089748z/Escriptori/IdeaProjects/DadesIServeis/src/UF3AAD/ExamenUF3/mondial.xml";
+    private static String XPathQuery = "/mondial/country/name";
+
+    //EL CODI DEL MAIN NO CAL TOCARLO I EL DEL ExistsDAO TAMPOC
     public static void main(String[] args)
     {
-     /* IP = "172.31.83.34";
-        adminUsername="admin";
-        adminPassword="admin";  */
         try
         {
-            //EJEMPLO DE CREAR COLLECCION
-            DAO.createCollection(driver, URI+mainCollection, adminUsername, adminPassword, yourCollection);
+            //CLASE PARA CREAR COLLECCION
+            existsDAO.createCollection(URI+mainCollection, adminUsername, adminPassword, yourCollection);
 
-            //EJEMPLO DE CREAR RECURSO
-            DAO.createResource(driver, URI+mainCollection+"/"+yourCollection, adminUsername, adminPassword, filePath);
+            //CLASE PARA CREAR RECURSO
+            existsDAO.createResource(URI+mainCollection+"/"+yourCollection, adminUsername, adminPassword, filePath);
 
-            //EJEMPLO DE CONSULTA XPATH
+            //CLASE PARA CONSULTA XPATH
             System.out.println("\nCONSULTA XPATH");
-            System.out.println(DAO.query("collection('"+yourCollection+"')/mondial/country/name", PORT, IP));
+            System.out.println(existsDAO.query("collection('"+yourCollection+"')"+XPathQuery, PORT, IP));
 
-            //EJEMPLO DE CONSULTA XQUERY
+            //CLASE PARA CONSULTA XQUERY
             System.out.println("\nCONSULTA XQUERY");
-            System.out.println(DAO.query("for $p in fn:doc(\"uriDAO/mondial.xml\")//country[population=max(//mondial/country/population)] return $p/name ", PORT, IP));
-
-            //MODIFICAR RECURSO
-            DAO.deleteResource();
-
+            System.out.println(existsDAO.query("for $p in fn:doc(\"uriDAO/mondial.xml\")//country[population=max(//mondial/country/population)] return $p/name ", PORT, IP));
         }
         catch (XMLDBException e)          {System.out.println("XMLDBException:         " + e);}
         catch (ClassNotFoundException e)  {System.out.println("ClassNotFoundException: " + e);}
--- src/UF4AAD/ExistsDAO.java
+++ src/UF4AAD/ExistsDAO.java
@@ -8,12 +8,10 @@ import org.xmldb.api.base.XMLDBException;
 import org.xmldb.api.modules.CollectionManagementService;
 import javax.xml.xquery.*;
 import java.io.File;
-/**
- * Created by 48089748z on 29/03/16.
- */
-public class DAO
+public class ExistsDAO
 {
-    public static void createCollection(String driver, String URI, String adminUsername, String adminPassword, String collectionName) throws XMLDBException, ClassNotFoundException, IllegalAccessException, InstantiationException
+    private static final String driver = "org.exist.xmldb.DatabaseImpl";
+    public void createCollection(String URI, String adminUsername, String adminPassword, String collectionName) throws XMLDBException, ClassNotFoundException, IllegalAccessException, InstantiationException
     {
         //CONEXIÓN
         Class clas = Class.forName(driver);
@@ -26,7 +24,7 @@ public class DAO
         CollectionManagementService c = (CollectionManagementService) parent.getService("CollectionManagementService", "1.0");
         c.createCollection(collectionName);
     }
-    public static void createResource(String driver, String URI, String adminUsername, String adminPassword, String filePath) throws XMLDBException, ClassNotFoundException, IllegalAccessException, InstantiationException
+    public void createResource(String URI, String adminUsername, String adminPassword, String filePath) throws XMLDBException, ClassNotFoundException, IllegalAccessException, InstantiationException
     {
         //CONEXIÓN
         Class clas = Class.forName(driver);
@@ -43,7 +41,7 @@ public class DAO
         resource.setContent(file);
         collection.storeResource(resource);
     }
-    public static String query(String query, String PORT, String IP) throws XQException
+    public String query(String query, String PORT, String IP) throws XQException
     {
         //CONEXIÓN
         XQDataSource source = new ExistXQDataSource();
@@ -65,9 +63,5 @@ public class DAO
         }
         connection.close();
         return resultados;
-    }
-    public static void deleteResource()
-    {
-
     }
 }
