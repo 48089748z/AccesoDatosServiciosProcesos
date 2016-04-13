009b38ee59a670b7bb176be9e37a2c2113e12aac
48089748z <48089748z@iespoblenou.org>
2016-04-13 16:59:16 +0200

    SecureClient

--- src/UF1PSP/SecureProtocols/SecureClient.java
+++ src/UF1PSP/SecureProtocols/SecureClient.java
@@ -1,12 +1,30 @@
 package UF1PSP.SecureProtocols;
 
+import javax.net.ssl.SSLServerSocketFactory;
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
 /**
  * Created by 48089748z on 13/04/16.
  */
 public class SecureClient
 {
-    public static void main(String[] args)
+    public static void main(String[] args) throws IOException
     {
+        SSLSocketFactory ssf = (SSLSocketFactory) SSLSocketFactory.getDefault();
+
+        SSLSocket cliente = (SSLSocket) ssf.createSocket();
+        InetSocketAddress address = new InetSocketAddress("localhost", 5556);
+        cliente.connect(address);
+
+        OutputStream os = cliente.getOutputStream();
+        String mensage = "HOLA";
+        os.write(mensage.getBytes());
+
+        cliente.close();
 
     }
 }
