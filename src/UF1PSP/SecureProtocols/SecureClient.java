ab19e1bbd4996d37b0ba73ffcc1e1db8dc376c2d
48089748z <48089748z@iespoblenou.org>
2016-04-13 17:41:19 +0200

    Secure Protocols Calculator finished

--- src/UF1PSP/SecureProtocols/SecureClient.java
+++ src/UF1PSP/SecureProtocols/SecureClient.java
@@ -1,17 +1,22 @@
 package UF1PSP.SecureProtocols;
-
-import javax.net.ssl.SSLServerSocketFactory;
 import javax.net.ssl.SSLSocket;
 import javax.net.ssl.SSLSocketFactory;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
+import java.util.Scanner;
 
 /**
  * Created by 48089748z on 13/04/16.
  */
 public class SecureClient
 {
+    //GENERAR EL ARCHIVO CON LAS LLAVES (EJECUTAR EL COMANDO SIGUIENTE EN LA CARPETA CORRESPONDIENTE)
+    //keytool -genkey -keystore yourkeysnames -keyalg RSA
+
+    //REFERENCIAR LOS .class
+    //java -Djavax.net.ssl.keyStore=oriolkeys -Djavax.net.ssl.trustStore=oriolkeys -Djavax.net.ssl.keyStorePassword=callapesat SecureServer
+
     public static void main(String[] args) throws IOException
     {
         SSLSocketFactory ssf = (SSLSocketFactory) SSLSocketFactory.getDefault();
@@ -19,12 +24,13 @@ public class SecureClient
         SSLSocket cliente = (SSLSocket) ssf.createSocket();
         InetSocketAddress address = new InetSocketAddress("localhost", 5556);
         cliente.connect(address);
-
         OutputStream os = cliente.getOutputStream();
-        String mensage = "HOLA";
-        os.write(mensage.getBytes());
 
+        //Y ara enviem la operacio al Server per que ens doni el resultat.
+        Scanner in = new Scanner(System.in);
+        System.out.println("\nIntroduce la Operacion en String con * + - / sin espacios");
+        String operacion = in.nextLine();
+        os.write(operacion.getBytes());
         cliente.close();
-
     }
 }
--- src/UF1PSP/SecureProtocols/SecureServer.java
+++ src/UF1PSP/SecureProtocols/SecureServer.java
@@ -3,6 +3,9 @@ package UF1PSP.SecureProtocols;
 import javax.net.ssl.SSLServerSocket;
 import javax.net.ssl.SSLServerSocketFactory;
 import javax.net.ssl.SSLSocket;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.InetSocketAddress;
@@ -12,11 +15,10 @@ import java.net.InetSocketAddress;
  */
 public class SecureServer
 {
-    public static void main(String[] args) throws IOException
+    public static void main(String[] args) throws IOException, ScriptException
     {
         SSLServerSocketFactory ssf = (SSLServerSocketFactory) SSLServerSocketFactory.getDefault();
         SSLServerSocket sss = (SSLServerSocket) ssf.createServerSocket();
-
         InetSocketAddress address = new InetSocketAddress("0.0.0.0", 5556);
         sss.bind(address);
 
@@ -25,7 +27,15 @@ public class SecureServer
         byte[] men = new byte[25];
         is.read(men);
 
-        System.out.println("Message: "+new String(men));
+        String msg = new String(men);
+        System.out.println("Resultado de: "+msg+" "+calcular(msg));
         sss.close();
+    }
+
+    public static String calcular(String msg) throws ScriptException
+    {
+        ScriptEngineManager mgr = new ScriptEngineManager();
+        ScriptEngine engine = mgr.getEngineByName("JavaScript");
+        return engine.eval(msg).toString();
     }
 }
